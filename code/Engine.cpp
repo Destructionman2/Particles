d08f5b97692e6a7fbc0d67d63c5914b913da08c2
Julio Escobar <W1950537@apps.losrios.edu>
2023-12-01 16:02:38 -0800

    Added Engine cpp file ( Forgot to add it in original version )

---
+++ code/Engine.cpp
@@ -0,0 +1,12 @@
+#include "code/Engine.h"  //Re-Adjust file opening if needed
+
+Engine::Engine()
+{
+	m_Window.create(sf::VideoMode::getDesktopMode(), "Particles!");
+}
+
+void Engine::run()
+{
+	Clock time;
+	Particle 
+}
